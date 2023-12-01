dedb3633ad53d36a87c39936906902c2564ace7d
Kenneth Yan <kennyan11b@gmail.com>
2023-12-01 15:57:41 -0800

    Feature: GameState Refactored

--- src/main/java/entities/GameState.java
+++ src/main/java/entities/GameState.java
@@ -1,12 +1,62 @@
 package main.java.entities;
 
 import java.util.Arrays;
+import main.java.ui.DifficultyLevel;
 
 public class GameState {
+    public static int NUM_ATTEMPTS = 10;
+    public static final int NUM_DIGITS = 4;
+    public static int MIN_VALUE = 1;
+    public static int MAX_VALUE = 8;
+
     private int[] secretCode;
     private int[] playerGuess;
     private int attemptsLeft;
     private boolean gameEnded;
+    private DifficultyLevel difficultyLevel;
+    private boolean twoPlayerMode;
+    private CodeGenerator codeGenerator;
+
+    public void resetGame() {
+        if (!twoPlayerMode) {
+            switch (difficultyLevel) {
+                case EASY:
+                    NUM_ATTEMPTS = 12;
+                    MIN_VALUE = 1;
+                    MAX_VALUE = 6;
+                    break;
+                case MEDIUM: // Default
+                    NUM_ATTEMPTS = 10;
+                    MIN_VALUE = 1;
+                    MAX_VALUE = 8;
+                    break;
+                case HARD:
+                    NUM_ATTEMPTS = 8;
+                    MIN_VALUE = 1;
+                    MAX_VALUE = 10;
+                    break;
+            }
+
+            codeGenerator = new CodeGenerator();
+            secretCode = codeGenerator.generateSecretCode();
+            attemptsLeft = NUM_ATTEMPTS;
+
+        }
+
+        playerGuess = new int[NUM_DIGITS];
+    }
+
+    public int getNumDigits() {
+        return NUM_DIGITS;
+    }
+
+    public int getMinValue() {
+        return MIN_VALUE;
+    }
+
+    public int getMaxValue() {
+        return MAX_VALUE;
+    }
 
     public int[] getSecretCode() {
         return secretCode;
@@ -32,18 +82,35 @@ public class GameState {
         this.attemptsLeft = attemptsLeft;
     }
 
-    public boolean isGameEnded() {
-        return gameEnded;
+    public void setDifficulty(DifficultyLevel difficulty) {
+        this.difficultyLevel = difficulty;
+    }
+
+    public void setTwoPlayerMode(boolean twoPlayerMode) {
+        this.twoPlayerMode = twoPlayerMode;
     }
 
     public void setGameEnded(boolean gameEnded) {
         this.gameEnded = gameEnded;
     }
 
+    public boolean isGameEnded() {
+        return gameEnded;
+    }
+
+    public boolean isTwoPlayerMode() {
+        return twoPlayerMode;
+    }
+
+    public void decrementAttemptsLeft() {
+        attemptsLeft--;
+    }
+
     @Override
     public String toString() {
         return "GameState [secretCode=" + Arrays.toString(secretCode) + ", playerGuess=" + Arrays.toString(playerGuess)
-                + ", attemptsLeft=" + attemptsLeft + ", gameEnded=" + gameEnded + "]";
+                + ", attemptsLeft=" + attemptsLeft + ", gameEnded=" + gameEnded + ", difficultyLevel=" + difficultyLevel
+                + ", twoPlayerMode=" + twoPlayerMode + ", codeGenerator=" + codeGenerator + "]";
     }
 
 }
--- src/main/java/ui/MenuHandler.java
+++ src/main/java/ui/MenuHandler.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 
 import main.java.app.GameController;
 import main.java.app.MastermindGame;
+import main.java.entities.GameState;
 import main.java.entities.Player;
 
 public class MenuHandler {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void printMenu(MastermindGame game, GameController gameController) {
+    public static void printMenu(GameState gameState, GameController gameController) {
         boolean gameEnded = false;
 
         while (!gameEnded) {
@@ -24,16 +25,16 @@ public class MenuHandler {
                     System.exit(0);
                     break;
                 case "1":
-                    game.resetGame();
+                    gameState.resetGame();
                     gameController.playGame();
                     break;
                 case "2":
-                    game.setTwoPlayerMode(true);
-                    game.resetGame();
-                    playTwoPlayer(game, gameController);
+                    gameState.setTwoPlayerMode(true);
+                    gameState.resetGame();
+                    playTwoPlayer(gameState, gameController);
                     break;
                 case "3":
-                    selectDifficulty(game);
+                    selectDifficulty(gameState);
                     break;
                 case "4":
                     GameView.printInstructions(scanner);
@@ -43,45 +44,46 @@ public class MenuHandler {
                     break;
             }
 
-            gameEnded = game.isGameEnded();
+            gameEnded = gameState.isGameEnded();
         }
     }
 
-    private static void selectDifficulty(MastermindGame game) {
+    private static void selectDifficulty(GameState gameState) {
         GameView.printDifficultyMenu();
 
         String difficultyChoice = scanner.nextLine();
 
         switch (difficultyChoice) {
             case "1":
-                game.setDifficulty(DifficultyLevel.EASY);
+                gameState.setDifficulty(DifficultyLevel.EASY);
                 System.out.println("Difficulty set to Easy");
                 break;
             case "2":
-                game.setDifficulty(DifficultyLevel.MEDIUM);
+                gameState.setDifficulty(DifficultyLevel.MEDIUM);
                 System.out.println("Difficulty set to Medium");
                 break;
             case "3":
-                game.setDifficulty(DifficultyLevel.HARD);
+                gameState.setDifficulty(DifficultyLevel.HARD);
                 System.out.println("Difficulty set to Hard");
                 break;
             default:
                 System.out.println("Invalid difficulty choice. Please enter a number (1, 2, or 3). ");
                 break;
         }
-        game.resetGame();
+        gameState.resetGame();
     }
 
     private static void handleInvalidChoice() {
         System.out.println("Invalid menu choice. Please enter a number between 1 and 4.");
     }
 
-    private static void playTwoPlayer(MastermindGame game, GameController gameController) {
-        int[] secretCode = Player.getPlayerGuess(game.getNumDigits(), game.getMinValue(), game.getMaxValue(), true);
+    private static void playTwoPlayer(GameState gameState, GameController gameController) {
+        int[] secretCode = Player.getPlayerGuess(gameState.getNumDigits(), gameState.getMinValue(),
+                gameState.getMaxValue(), true);
 
-        game.setSecretCode(secretCode);
+        gameState.setSecretCode(secretCode);
         gameController.playGame();
-        game.setGameEnded(true);
+        gameState.setGameEnded(true);
     }
 
 }
